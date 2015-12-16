047f23c2befa56ac642fafb2114b3835713b758a
Rick Patci <rpatci@gmail.com>
2015-12-15 23:41:09 -0800

    setup karma.conf and CRUD tests

--- gulpfile.js
+++ gulpfile.js
@@ -1,4 +1,5 @@
 var gulp = require('gulp');
+var mocha = require('gulp-mocha');
 var webpack = require('webpack-stream');
 var sass = require('gulp-sass');
 var minifyCSS = require('gulp-minify-css');
@@ -12,6 +13,28 @@ gulp.task('cssFiles:dev', function() {
   gulp.src('app/scss/*.scss')
   .pipe(sass())
   .pipe(gulp.dest('build/css/'));
+});
+
+gulp.task('webpack:test', function() {
+  return gulp.src('./test/client/test_entry.js')
+    .pipe(webpack({
+      output: {
+        filename: 'test_bundle.js'
+      }
+    }))
+    .pipe(gulp.dest('test/client'));
+});
+
+gulp.task('servertests', function() {
+  return gulp.src('./test/server/**/*tests.js')
+    .pipe(mocha({reporter: 'nyan'}))
+    .once('error', function() {
+      process.exit();
+    })
+    .once('end', function() {
+      if (this.seq.length === 1 && this.seq[0] === 'servertests')
+        process.exit();
+    }.bind(this));
 });
 
 gulp.task('jsFiles:dev', function() {
--- test/client/chords_controller_tests.js
+++ test/client/chords_controller_tests.js
@@ -1,1 +1,2 @@
+require('/../../app/js/client');
 require('angular-mocks');
