63ec035b34ab6c54c29e40d7068324d290653ea1
Rick Patci <rpatci@gmail.com>
2015-12-16 14:28:14 -0800

    add travis update package.json

---
+++ app/js/entry.js
@@ -0,0 +1,9 @@
+require('angular/angular');
+require('angular-route');
+require('angular-cookies');
+require('angular-base64');
+
+var angular = window.angular;
+
+var songwriterApp = angular.module('SongwriterApp', ['ngRoute', 'ngCookies', 'base64']);
+
