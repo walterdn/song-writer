0f1184c23213c14c733b0e0aa6d54588264eb7be
Rick Patci <rpatci@gmail.com>
2015-12-17 14:53:04 -0800

    add signup_controller.js

---
+++ app/js/auth/controllers/signup_controller.js
@@ -0,0 +1,20 @@
+module.exports = function(app) {
+  app.controller('SignupController', ['$scope', '$http', '$cookies', '$location', function($scope, $http, $cookies, $location) {
+    $scope.headingText = 'Signup to create sweet jams';
+    $scope.buttonText = 'Create User';
+    $scope.authenticate = function(user) {
+      $http.post('/api/signup', user)
+        .then(function(res){
+          $cookies.put('token', res.data.token);
+          $scope.getUser();
+          $location.path('/songs');
+        }, function(err) {
+          console.log(err.data);
+        });
+    };
+
+    $scope.changePlaces = function() {
+      $location.path('/signin');
+    };
+  }]);
+};
