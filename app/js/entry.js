9a1dbe2422385ae862709255a1d9a51e990e304b
Rick Patci <rpatci@gmail.com>
2015-12-17 14:08:20 -0800

    first commit of new journey!

--- app/js/entry.js
+++ app/js/entry.js
@@ -2,8 +2,36 @@ require('angular/angular');
 require('angular-route');
 require('angular-cookies');
 require('angular-base64');
-
 var angular = window.angular;
 
-var songwriterApp = angular.module('SongwriterApp', ['ngRoute', 'ngCookies', 'base64']);
+var songWriterApp = angular.module('SongWriterApp', ['ngRoute', 'ngCookies', 'base64']);
+require('./filters/filters')(songWriterApp);
+require('./services/services')(songWriterApp);
+require('./controllers/controllers')(songWriterApp);
+require('./directives/directives')(songWriterApp);
 
+require('./songs/songs')(songWriterApp);
+require('./auth/auth')(songWriterApp);
+
+songWriterApp.config(['$routeProvider', function($route) {
+  $route
+    .when('/songs', {
+      templateUrl: '/templates/songs_view.html',
+      controller: 'SongsController'
+    })
+    .when('/signup', {
+      templateUrl: '/templates/auth_view.html',
+      controller: 'SignupController'
+    })
+    .when('/signin', {
+      templateUrl: '/templates/auth_view.html',
+      controller: 'SigninController'
+    })
+    .when('/allsongs', {
+      templateUrl: '/templates/all_songs.html',
+      controller: 'AllSongsController'
+    })
+    .otherwise({
+      redirectTo: '/signup'
+    })
+}]);
