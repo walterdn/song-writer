8ddae2a06b6b19ae93eb82afeae15de2b560a17f
Rick Patci <rpatci@gmail.com>
2015-12-17 18:10:50 -0800

    update song model

--- models/song.js
+++ models/song.js
@@ -3,7 +3,8 @@ var mongoose = require('mongoose');
 var songSchema = new mongoose.Schema({
   name: String,
   chords: Array,
-  author: String
+  author: String,
+  melody: Array
 });
 
 module.exports = mongoose.model('Song', songSchema);
